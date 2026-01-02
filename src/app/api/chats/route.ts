4d374e8f1cdd674d2d96fafd31f68774f25290ee
TanvirDF <ahamedtanvir619@gmail.com>
2026-01-02 16:05:28 +0000

    next js fix

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,41 +1,48 @@
 const BACKEND_URL = process.env.BACKEND_URL || "";
 export async function POST(req: Request) {
-  const { message } = await req.json();
-  const chatBackendResponse = await fetch(`${BACKEND_URL}/chats?stream=true`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ userQuery: message }),
-  });
-  if (!chatBackendResponse.ok) {
-    return new Response("Error from chat backend", { status: 500 });
-  }
-  const stream = new ReadableStream({
-    async start(controller) {
-      const reader = chatBackendResponse.body?.getReader();
-      const decoder = new TextDecoder();
-      if (!reader) {
-        controller.close();
-        return;
+  try {
+    const { message } = await req.json();
+    const chatBackendResponse = await fetch(
+      `${BACKEND_URL}/chats?stream=true`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userQuery: message }),
       }
-      while (true) {
-        const { done, value } = await reader.read();
-        if (done) {
-          break;
+    );
+    if (!chatBackendResponse.ok) {
+      return new Response("Error from chat backend", { status: 500 });
+    }
+    const stream = new ReadableStream({
+      async start(controller) {
+        const reader = chatBackendResponse.body?.getReader();
+        const decoder = new TextDecoder();
+        if (!reader) {
+          controller.close();
+          return;
         }
-        const text = decoder.decode(value);
-        controller.enqueue(text);
-      }
-      controller.close();
-    },
-  });
-  return new Response(stream, {
-    headers: {
-      "Content-Type": "application/x-ndjson",
-      // "Content-Type": "text/event-stream",
-      "Transfer-Encoding": "chunked",
-      "Cache-Control": "no-cache",
-    },
-  });
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) {
+            break;
+          }
+          const text = decoder.decode(value);
+          controller.enqueue(text);
+        }
+        controller.close();
+      },
+    });
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "application/x-ndjson",
+        // "Content-Type": "text/event-stream",
+        "Transfer-Encoding": "chunked",
+        "Cache-Control": "no-cache",
+      },
+    });
+  } catch {
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
 // export async function POST(req: Request) {
 //   const { message } = await req.json();
